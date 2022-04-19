c4c386f284a7e96c45318870a60c166ee5b78655
Ddiiro Joseph <ddiiro92@gmail.com>
2022-04-19 18:44:06 +0300

    UI of a sign up form using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export function Nav() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/">About</a>
+      </li>
+      <li>
+        <a href="/">Services</a>
+      </li>
+      <li>
+        <a href="/">Contact</a>
+      </li>
+    </ul>
   );
 }
 
-export default App;
+export function Title() {
+  return <h1>SignUp Form</h1>;
+}
+
+export function Paragraph() {
+  return <p>Please signup for a new account here.</p>;
+}
+
+export function Form() {
+  return (
+    <form>
+      <label>Surname</label>
+      <br></br>
+      <input></input>
+      <br></br>
+      <label>Lastname</label>
+      <br></br>
+      <input></input>
+      <br></br>
+      <label>Email</label>
+      <br></br>
+      <input></input>
+      <br></br>
+      <label>Phone Number</label>
+      <br></br>
+      <input></input> <br></br>
+      <label>Password</label>
+      <br></br>
+      <input></input> <br></br>
+      <label>Confirm Password</label>
+      <br></br>
+      <input></input> <br></br>
+      <button>SignUp</button>
+    </form>
+  );
+}
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import{Nav,Title,Paragraph,Form,} from './App'
+
+ReactDom.render(<Nav/>, document.getElementById('nav'))
+ReactDom.render(<Title/>, document.getElementById('title'))
+ReactDom.render(<Paragraph/>, document.getElementById('paragraph'))
+ReactDom.render(<Form/>, document.getElementById('root'))
